d1c9f88589dbbf730863d52ae065788566438fe7
well-balanced <ws.kim@specupad.com>
2020-03-02 21:56:26 +0900

    Study Buble Sort

---
+++ buble_sort.py
@@ -0,0 +1,13 @@
+def swap(x, i, j):
+    x[i], x[j] = x[j], x[i]
+
+def buble_sort(numbers):
+    print(numbers)
+    for labs in reversed(range(len(numbers))):
+        for i in range(labs):
+            if numbers[i] > numbers[i+1]:
+                swap(numbers, i, i+1)
+    print(numbers)
+
+numbers = [3, 7, 4, 5, 2, 6, 1]
+buble_sort(numbers)
